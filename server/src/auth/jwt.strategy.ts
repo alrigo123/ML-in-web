25f35ee8d81ed338522a10857d7d8a1ef22fe70d
alrigo123 <alexestrada2804@gmail.com>
2025-03-10 12:21:47 -0500

    JWT

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/await-thenable */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: 'SECRET_KEY', // ¡Usa una variable de entorno en producción!
+        });
+    }
+
+    async validate(payload: any) {
+        return await { userId: payload.sub, email: payload.email };
+    }
+}
