d0003ee244e5ee43f9c7297c5125210e75bf9dd7
alrigo123 <alexestrada2804@gmail.com>
2025-03-21 12:02:52 -0500

    TASKS RESNET50

--- RESNET50.sh
+++ RESNET50.sh
@@ -7,8 +7,6 @@ https://www.kaggle.com/datasets/mohammedmaazshaikh/formula-1-championship-datase
 https://ergast.com/mrd/ API F1
 
 # MODELS TO TEST
-https://medium.com/@nitishkundu1993/exploring-resnet50-an-in-depth-look-at-the-model-architecture-and-code-implementation-d8d8fa67e46f
-https://medium.com/@onur_andros_ozbek/food-101-classifier-using-resnet50-on-colab-f0cc6ac6487
 https://www.kaggle.com/code/aakhlopov/simple-custom-resnet-with-colab
 https://www.kaggle.com/code/farzadnekouei/imbalanced-garbage-classification-resnet50/input
 https://www.kaggle.com/code/gaborfodor/resnet50-example/notebook
