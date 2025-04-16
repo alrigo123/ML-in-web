bb4c55beb49aa1bcf226c76838f3c4914e19e3cf
alrigo123 <alexestrada2804@gmail.com>
2025-04-16 17:19:09 -0500

    CHANGES

--- EFFICIENT_NET.sh
+++
@@ -1,5 +0,0 @@
-https://colab.research.google.com/github/google/automl/blob/master/efficientnetv2/tutorial.ipynb
-
-https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/vision/ipynb/image_classification_efficientnet_fine_tuning.ipynb
-
-https://colab.research.google.com/github/KiweeEu/transfer-learning-demo/blob/master/TransferLearning.ipynb
---
+++ Robot/EfficientNet/EFFICIENT_NET.sh
@@ -0,0 +1,11 @@
+https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/vision/ipynb/image_classification_efficientnet_fine_tuning.ipynb
+
+https://colab.research.google.com/github/KiweeEu/transfer-learning-demo/blob/master/TransferLearning.ipynb
+
+
+# INFO
+https://blog.roboflow.com/what-is-efficientnet/
+https://arxiv.org/pdf/1905.11946
+https://arjun-sarkar786.medium.com/understanding-efficientnet-the-most-powerful-cnn-architecture-eaeb40386fad
+https://keras.io/api/applications/efficientnet/
+https://github.com/tensorflow/tpu/tree/master/models/official/efficientnet
