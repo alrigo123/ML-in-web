5d6ee9726763ace9c23e03961f0ed6d97b2b3e27
alrigo123 <alexestrada2804@gmail.com>
2025-06-24 08:03:12 -0500

    remove task.sh file and associated tasks

--- task.sh
+++
@@ -1,9 +0,0 @@
-[x] que llamando del server 7000 se pueda ver lo que manda el 8000 (python)
-[x] mostrar en la interfaz
-[x] luego cambiar el input en lugar de 10 que sea N y la variable
-{X} Ruta y CRUD modelos que funcione
-{x} Login y register Usuario ('logica y estetica') (https://bluuweb.dev/nestjs/auth-jwt.html)
-{} Implementar modelos
-[] mostrar en la interfaz los resultados en graficas t tambien las predicciones en tabla asi
-[] login y register XD ("para que cuando le den entrenar al modelo se vea quien lo echo")
-[] CRUD delete model, tambien se borre su interfaz o algo asi
